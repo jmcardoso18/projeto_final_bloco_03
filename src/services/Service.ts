c0769d03437a66dd18856f1b42893d1c3223fdd3
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-12-02 16:25:05 -0300

    Desisti do Deploy, mais fácil criar um novo repositório, com as pastas com nome certo e subi por ele, voltando as configurações sem deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL
+   baseURL:'https://farmacia-jk1x.onrender.com/'
   })
   
   export const listar = async(url: string, setDados: Function) => {
